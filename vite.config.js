e0f04f88d3868bbbd257f3ecf8175929fdea2eef
Kishor Kukreja <kishorkukreja76@gmail.com>
2024-11-05 21:56:25 +0000

    using latest changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/frosty-paper/",
   build: {
     rollupOptions: {
       external: [], // Leave empty or specify dependencies you truly want externalized
