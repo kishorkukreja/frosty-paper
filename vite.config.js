caf79ad1f5fb6973c8684ec031bfdc46501dd1e6
Kishor Kukreja <kishorkukreja76@gmail.com>
2024-11-09 18:23:05 +0000

    modified vite

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
