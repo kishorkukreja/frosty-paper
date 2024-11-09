778da48a9f017e4cbc8793408eaec00368789989
Kishor Kukreja <kishorkukreja76@gmail.com>
2024-11-09 18:32:40 +0000

    Added lazy routes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
-    assetsDir: "assets",
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: undefined,
